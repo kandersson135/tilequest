e8957f13f1f87cacfad36e0d9dbb9ad42bbe4339
kandersson135 <115895961+kandersson135@users.noreply.github.com>
2023-06-08 19:49:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
     let level = 1;
     const boardSize = 15; // Fixed number of square tiles in each row
     let numEnemies = 3 + level; // Number of enemies on the board
-    let maxLives = 5; // Maximum lives for the teacher
+    let maxLives = 5; // Maximum lives for the hero
 
-    let teacherPosition = { row: 0, col: 0 }; // Initial teacher position
+    let heroPosition = { row: 0, col: 0 }; // Initial hero position
     let lives = maxLives; // Current number of lives
     let enemies = []; // Array to store enemy positions
 	
@@ -12,6 +12,19 @@ $(document).ready(function() {
 	let stepSound = new Audio("audio/step.wav");
 	let hurtSound = new Audio("audio/hurt.mp3");
 	let levelUpSound = new Audio("audio/level-up.wav");
+	
+	let enemyImages = [
+		'url(img/green-slime.gif)',
+		'url(img/pink-slime.gif)',
+		'url(img/white-slime.gif)',
+		'url(img/yellow-slime.gif)',
+		'url(img/zombie.gif)',
+		'url(img/green-slime.gif)',
+		'url(img/pink-slime.gif)',
+		'url(img/white-slime.gif)',
+		'url(img/yellow-slime.gif)',
+		'url(img/ogre.gif)'
+	];
 	
 	swordSound.volume = 0.3;
 	stepSound.volume = 0.3;
@@ -24,8 +37,8 @@ $(document).ready(function() {
     // Generate enemies at random positions
     generateEnemies();
 
-    // Add teacher to the initial position
-    updateTeacherPosition();
+    // Add hero to the initial position
+    updateHeroPosition();
     
     // Generate lives
     updateLives();
@@ -39,23 +52,23 @@ $(document).ready(function() {
         const clickedCol = $(this).data('col');
         const newPosition = { row: clickedRow, col: clickedCol };
 
-        if (isValidPosition(newPosition) && isAdjacentPosition(newPosition, teacherPosition)) {
-            teacherPosition = newPosition;
-            updateTeacherPosition();
+        if (isValidPosition(newPosition) && isAdjacentPosition(newPosition, heroPosition)) {
+            heroPosition = newPosition;
+            updateHeroPosition();
             moveEnemiesCloser();
 			stepSound.play();
 
             checkCollisions();
-        } else if (isInRangeOfAttack(newPosition, teacherPosition)) {
-			$('.teacher').css('background-image','url(img/hero-attack.gif)');
-			$('.teacher').css('background-size','auto');
+        } else if (isInRangeOfAttack(newPosition, heroPosition)) {
+			$('.hero').css('background-image','url(img/hero-attack.gif)');
+			$('.hero').css('background-size','auto');
 			
 			setTimeout(function() { 
 				swordSound.play();
 			}, 200);
 			
 			setTimeout(function() { 
-				$('.teacher').css('background-image','url(img/hero-idle.gif)');
+				$('.hero').css('background-image','url(img/hero-idle.gif)');
 				
 				killEnemy(newPosition);
 			}, 600);
@@ -71,6 +84,17 @@ $(document).ready(function() {
             }
         }
     }
+	
+	function getEnemyImage(level) {
+		let index = level - 1;
+
+		if (index >= 0 && index < enemyImages.length) {
+			return enemyImages[index];
+		}
+
+		// Return a default image if the level is out of range
+		return 'url(img/green-slime.gif)';
+	}
 
     function generateEnemies() {
         for (let i = 0; i < numEnemies; i++) {
@@ -94,18 +118,19 @@ $(document).ready(function() {
         return (rowDiff === 2 && colDiff === 0) || (rowDiff === 0 && colDiff === 2);
     }
 
-    function updateTeacherPosition() {
-        $('.teacher').remove();
-        $(`.tile[data-row=${teacherPosition.row}][data-col=${teacherPosition.col}]`).append('<div class="teacher"></div>');
+    function updateHeroPosition() {
+        $('.hero').remove();
+        $(`.tile[data-row=${heroPosition.row}][data-col=${heroPosition.col}]`).append('<div class="hero"></div>');
     }
 
     function moveEnemiesCloser() {
         enemies.forEach(function(enemy, index) {
-            const newPosition = getNewPosition(enemy, teacherPosition);
+            const newPosition = getNewPosition(enemy, heroPosition);
+			const currentLevelEnemyImage = getEnemyImage(level);
             if (isValidPosition(newPosition)) {
                 enemies[index] = newPosition;
                 $(`.tile[data-row=${enemy.row}][data-col=${enemy.col}] .enemy`).remove();
-                $(`.tile[data-row=${newPosition.row}][data-col=${newPosition.col}]`).append('<div class="enemy"></div>');
+                $(`.tile[data-row=${newPosition.row}][data-col=${newPosition.col}]`).append('<div class="enemy" style="background-image:' + currentLevelEnemyImage + ';"></div>');
             }
         });
     }
@@ -120,9 +145,9 @@ $(document).ready(function() {
     }
 
     function checkCollisions() {
-        // Check if the teacher and any enemy occupy the same position
+        // Check if the hero and any enemy occupy the same position
         enemies.forEach(function(enemy) {
-            if (enemy.row === teacherPosition.row && enemy.col === teacherPosition.col) {
+            if (enemy.row === heroPosition.row && enemy.col === heroPosition.col) {
                 lives--;
                 updateLives();
 				hurtSound.play();
@@ -138,7 +163,7 @@ $(document).ready(function() {
 					
                     resetGame();
                 } else {
-                    // Remove the enemy if it catches the teacher
+                    // Remove the enemy if it catches the hero
                     const index = enemies.findIndex(e => e.row === enemy.row && e.col === enemy.col);
                     if (index !== -1) {
                         enemies.splice(index, 1);
@@ -148,7 +173,7 @@ $(document).ready(function() {
             }
         });
 
-        if (teacherPosition.row === boardSize - 1 && teacherPosition.col === boardSize - 1) {
+        if (heroPosition.row === boardSize - 1 && heroPosition.col === boardSize - 1) {
             // Level up condition
             if (level === 10) {
                 // Final level completed
@@ -190,10 +215,13 @@ $(document).ready(function() {
             enemy = generateRandomPosition();
         } while (
             enemies.some(e => e.row === enemy.row && e.col === enemy.col) ||
-            (enemy.row === teacherPosition.row && enemy.col === teacherPosition.col)
+            (enemy.row === heroPosition.row && enemy.col === heroPosition.col)
         );
+		
+		const currentLevelEnemyImage = getEnemyImage(level);
+		
         enemies.push(enemy);
-        $(`.tile[data-row=${enemy.row}][data-col=${enemy.col}]`).append('<div class="enemy"></div>');
+        $(`.tile[data-row=${enemy.row}][data-col=${enemy.col}]`).append('<div class="enemy" style="background-image:' + currentLevelEnemyImage + ';"></div>');
     }
 
     function generateRandomPosition() {
@@ -211,7 +239,7 @@ $(document).ready(function() {
         numEnemies = 3 + level;
         //lives = maxLives;
         updateLives();
-        teacherPosition = { row: 0, col: 0 };
+        heroPosition = { row: 0, col: 0 };
         enemies = [];
 
         $('.tile').empty();
@@ -221,7 +249,7 @@ $(document).ready(function() {
         // Generate enemies at random positions
         generateEnemies();
 
-        updateTeacherPosition();
+        updateHeroPosition();
     }
 
     function resetGame() {
@@ -229,7 +257,7 @@ $(document).ready(function() {
         numEnemies = 3 + level;
         lives = maxLives;
         updateLives();
-        teacherPosition = { row: 0, col: 0 };
+        heroPosition = { row: 0, col: 0 };
         enemies = [];
 
         $('.tile').empty();
@@ -239,6 +267,6 @@ $(document).ready(function() {
         // Generate enemies at random positions
         generateEnemies();
 
-        updateTeacherPosition();
+        updateHeroPosition();
     }
 });
